86de71e3dbc4b1bd14d765048f779da6501cd0f5
jjohn92085 <you@example.com>
2023-07-14 20:10:19 -0700

    calculator

--- Balloon/script.js
+++
@@ -1,8 +0,0 @@
-function toggleBalloon() {
-    let balloon = document.querySelector('.balloon');
-    if (balloon.style.display === 'none') {
-        balloon.style.display = 'block';
-    } else {
-        balloon.style.display = 'none';
-    }
-}
