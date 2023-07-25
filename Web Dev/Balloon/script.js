36b4d9c83e81202efba699165cd2515116ae1785
jjohn92085 <you@example.com>
2023-07-24 19:12:26 -0700

    edits to balloon

--- Web Dev/Balloon/script.js
+++ Web Dev/Balloon/script.js
@@ -1,10 +1,31 @@
-const balloon = document.getElementById("balloon")
-
-balloon.addEventListener("click", () => {
-    //adds animation
-    balloon.style.animation = "pop 0.3s linear";
-    //removes balloon from the DOM
+function popBalloon() {
+    // Gets the balloon element
+    const balloon = document.getElementById('balloon');
+  
+    // adds the popped class
+    balloon.classList.add('popped');
+  
+    // removes the balloon from the DOM
     setTimeout(() => {
-        balloon.remove();
-    }, 300);
-});
+      balloon.remove();
+    }, 1000); 
+
+    //adds the balloon to the DOM
+
+    setTimeout(() => {
+         // Creates a div
+        const div = document.createElement('div');
+        div.id = 'balloon'
+
+        // Get the container where you want to add the new element
+    const container = document.getElementById('container');
+
+    // Append the new element to the container
+    container.appendChild(div);
+
+
+
+    }, 3000)
+
+  }
+  
