413728cfaee76d70a70cc6f6b47f61e2856a4086
jjohn92085 <you@example.com>
2023-09-17 14:46:51 -0700

    cleaned up repo

---
+++ Python/namescrambler.py
@@ -0,0 +1,18 @@
+# import the built-in random module for Python
+import random
+
+# get user input
+
+input = input("Please enter your name")
+
+# make the string input into a list
+
+list = list(input)
+
+# convert to a list
+
+random.shuffle(list)
+
+# the shuffled list is converted back into a string
+
+scrambled = "".join(input_list)
